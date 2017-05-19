a9e42e712beeb134ca0a39b7024cc2ba11404b5c
Andre <post.for.andre@gmail.com>
2017-05-19 14:20:14 +0000

    Add websocket

--- camera_test.py
+++ camera_test.py
@@ -3,10 +3,19 @@ import pygame
 import os
 import os.path
 from subprocess import call  
+from websocket import create_connection
+
+ws = create_connection("ws://agilegw.local:8080/ws/device/dummy001122334455.DummyData/subscribe")
+print "Receiving..."
+result =  ws.recv()
+print "Received '%s'" % result
+ws.close()
 
 
 
-camera = picamera.PiCamera()
-camera.capture('test_0004.png')
 
-call(["bash", "processing_kim"])
+
+#camera = picamera.PiCamera()
+#camera.capture('test_0004.png')
+
+#call(["bash", "processing_kim"])
