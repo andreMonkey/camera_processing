5bf2a01fa5dc919f036ab6e07bfb546d155e2fa8
Andre <post.for.andre@gmail.com>
2017-06-02 16:38:09 +0000

    Update camera_test.py to read and log sensor value and take picture

--- camera_test.py
+++ camera_test.py
@@ -7,16 +7,40 @@ import json
 import time
 import requests
 
+pictureId = 0
+
 while 1:
+	pictureId = pictureId + 1
+	sensorValue = get_sensor_data()
+	logSensorData(sensorValue)
+	takePicture(pictureId)
+	#pixelsortPicture()
+
+
+  
+def get_sensor_data():
 	#try latest update http request	
 	result = requests.get("http://agilegw.local:2000/api/device/dummy001122334455/lastUpdate").text  # try string replace for the "["
 		
 	#Data Format '[{"deviceID":"dummy001122334455","componentID":"DummyData","value":"24","unit":"dum","format":"","lastUpdate":1495723678802}]'
 	result = json.loads(result)
-	print result[0]['value']
+	value = result[0]['value']
+	print("Sensor value")
+	print value
 	
+	return (value)
 
-#camera = picamera.PiCamera()
-#camera.capture('test_0004.png')
+def takePicture(pictureId):
+	fileName = 'test_000'+ pictureId + 'png'
+	print("taking picture #", fileName)
+	camera = picamera.PiCamera()
+	camera.capture('test_0004.png')
+	
+def logSensorData(sensorValue):
+	with open("data_for_processing", "w") as text_file:
+		text_file.write( "value: {0}".sensorValue)
+		print("did logging to text-file")
 
-#call(["bash", "processing_kim"])
+def pixelsortPicture():
+	call(["bash", "processing_kim"])
+	
