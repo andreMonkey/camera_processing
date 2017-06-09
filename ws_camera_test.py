aeaf1e932d36db83f1e55a05bd880f76b07c702c
Andre <post.for.andre@gmail.com>
2017-06-09 12:36:51 +0000

    Try WS again

---
+++ ws_camera_test.py
@@ -0,0 +1,22 @@
+import picamera
+import pygame
+import os
+import os.path
+from subprocess import call  
+from websocket import create_connection
+
+ws = create_connection("ws://agilegw.local:8080/ws/device/dummy001122334455.DummyData/subscribe")
+while 1:
+  print "Receiving..."
+  result =  ws.recv()
+  print "Received '%s'" % result
+#ws.close()
+
+
+
+
+
+#camera = picamera.PiCamera()
+#camera.capture('test_0004.png')
+
+#call(["bash", "processing_kim"])
