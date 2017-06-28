13aa92c27e8c2042e82791b3041af8b55123fb41
Andre <post.for.andre@gmail.com>
2017-06-28 20:04:03 +0000

    Use setup as in test_film.py Code still dirty and full of debug but concept works

--- filming.py
+++ filming.py
@@ -6,26 +6,37 @@ import datetime as dt
 import RPi.GPIO as GPIO
 
 #GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BOARD)
+
+#GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+#camera = PiCamera()
+# pin-layout
 GPIO.setmode(GPIO.BOARD)
+# Pin 18 (GPIO 24) as input
+GPIO.setup(15, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+# recording flag
+isRecording = 0
+print(isRecording)
 
-GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-
-
-def film():
-    GPIO.add_event_detect(15, GPIO.RISING, callback=button_event_handler, bouncetime=300)    
-    camera = picamera.PiCamera(resolution=(1280, 720), framerate=24)
-    camera.rotation = 270
-    camera.start_preview()
-    camera.annotate_background = picamera.Color('red')
-    # timestamp camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    camera.annotate_text = 'PULSE: ' + str(get_data.get_pulse_data_from_websocket())
-    camera.start_recording('timestamped.h264')
-    start = dt.datetime.now()
-    while True:
-        # IDEA : for several sensor flip through the different sensors in a loop
+def film(isRecording):
+    #GPIO.add_event_detect(15, GPIO.RISING, callback=button_event_handler, bouncetime=300)    
+    if (GPIO.input(15) == GPIO.HIGH) and (isRecording == 0):
+        isRecording = 1
+        camera = picamera.PiCamera(resolution=(1280, 720), framerate=24)
+        camera.rotation = 270
+        camera.start_preview()
+        camera.annotate_background = picamera.Color('red')
+        # timestamp camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         camera.annotate_text = 'PULSE: ' + str(get_data.get_pulse_data_from_websocket())
-        camera.wait_recording(0.2)
+        camera.start_recording('timestamped.h264')
+        start = dt.datetime.now()
+    if (GPIO.input(15) == GPIO.LOW) and (isRecording == 1):
+        # loop waiting for the 'stop signal'
+        print("stoop")
+        camera.stop_recording
+#        isRecording = 0
+
     #camera.stop_recording()    
 
 def button_event_handler():
@@ -33,6 +44,12 @@ def button_event_handler():
     camera.stop_recording()
     exit()    
 
+while True:
+    # IDEA : for several sensor flip through the different sensors in a loop
+    if (isRecording == 1):
+        camera.annotate_text = 'PULSE: ' + str(get_data.get_pulse_data_from_websocket())
+        camera.wait_recording(0.2)
+    film(isRecording)
 
 
-film()
+#film()
--- test_film.py
+++ test_film.py
@@ -1,24 +1,37 @@
 import RPi.GPIO as GPIO
 from picamera import PiCamera
 from time import sleep
+import datetime as dt
 
 camera = PiCamera()
 # pin-layout
 GPIO.setmode(GPIO.BOARD)
 # Pin 18 (GPIO 24) as input
-GPIO.setup(18, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(15, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 # recording flag
 isRecording = 0
 
 # loop
 while 1 :
     # read input
-    if (GPIO.input(18) == GPIO.HIGH) and (isRecording == 0):
+    if (GPIO.input(15) == GPIO.HIGH) and (isRecording == 0):
         # if input = high start recording
-        camera.start_recording('video.h264')
+        
+        #camera.start_recording('video.h264')
+        camera.rotation = 270
+        camera.start_preview()
+        #camera.annotate_background = picamera.Color('red')
+        # timestamp camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        #camera.annotate_text = 'PULSE: ' + str(get_data.get_pulse_data_from_websocket())
+        camera.start_recording('timestamped.h264')
+        start = dt.datetime.now()
         isRecording = 1
+        print("record")
 
-    if (GPIO.input(18) == GPIO.LOW) and (isRecording == 1):
+    if (GPIO.input(15) == GPIO.LOW) and (isRecording == 1):
         # loop waiting for the 'stop signal'
-        camera.stop_recording
+        print("stoop")
+        camera.stop_recording()
+        #camera_stop_review()
+        #exit()
         isRecording = 0
